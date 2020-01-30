379709086ec4511b8cf6d340fecdd9d0cf19fef1
Júlio Ramos <ps2.ramos@gmail.com>
2020-01-29 21:18:42 -0300

    Implementação da tela "Sobre"
    Motivo: I (M - Melhoria | C - Correção | I - Implementação)
    
    Descrição:
      - Adicionadas funcionalidades para abertura da tela "Sobre".
      - Implementada uma classe para lidar com possíveis alertas do projeto.
    
    Observações:
      - Aproveitei para mover alguns arquivos de pacotes a fim de melhorar a
      organização do projeto.
    
    Referência:

--- src/gui/controllers/MainController.java
+++ src/gui/controllers/MainController.java
@@ -1,11 +1,20 @@
 package gui.controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainController implements Initializable {
 
@@ -30,13 +39,29 @@ public class MainController implements Initializable {
 
 	@FXML
 	public void onSobreMenuItemAction() {
-		System.out.println("Ajuda item click");
+		loadView("/gui/views/Sobre.fxml");
 	}
 
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
 		// TODO Auto-generated method stub
+	}
 
+	private synchronized void loadView(String absoluteName) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+			Scene mainScene = Main.getMainScene();
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			
+			Node mainMenu = mainVBox.getChildren().get(0);
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+			
+		} catch (IOException ex) {
+			Alerts.showAlert("Exception occurred", "Error loading view", ex.getMessage(), AlertType.ERROR);
+		}
 	}
 
 }
